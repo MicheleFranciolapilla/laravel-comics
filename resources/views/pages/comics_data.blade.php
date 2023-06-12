1e7e3f3a332021a45a937d4ad121e30687728447
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-06-12 12:06:25 +0900

    Ultimato. Mancano commenti & readme.md

--- resources/views/pages/comics_data.blade.php
+++ resources/views/pages/comics_data.blade.php
@@ -27,10 +27,34 @@
 @endsection
 
 @section('main_section')
-    <div id="icons_menu_area">
-    </div>
     <div id="single_card_view">
-
+        <div id="blue_strip">
+            <div id="thumb">
+                <img src="{{ $single_data['thumb'] }}" alt="{{ $single_data['title'] }}">
+                <h6 class="text-white-50 text-center">VIEW GALLERY</h6>
+            </div>
+        </div>
+        <div id="single_card" class="central">
+            <h2>{{ $single_data['title'] }}</h2>
+            <div id="data_block" class="d-flex my-3">
+                <h6 class="py-1 px-2 border border-1 border-dark text-black bg-info">SERIES: <span class="text-secondary">{{ $single_data['series'] }}</span></h6>
+                <h6 class="py-1 px-2 border border-1 border-dark text-black bg-info">TYPE: <span class="text-secondary">{{ $single_data['type'] }}</span></h6>
+                <h6 class="py-1 px-2 border border-1 border-dark text-black bg-info">U.S. PRICE: <span class="text-secondary">{{ $single_data['price'] }}</span></h6>
+            </div>
+            <p>{{ $single_data['description'] }}</p>
+            <hr>
+            <h6 class="text-black">SALE DATE: <span class="text-secondary">{{ $single_data['sale_date'] }}</span></h6>
+            <div id="authors" class="d-flex justify-content-between column-gap-3 w-100 border border-1 border-black rounded-1 p-2">
+                <div id="artists">
+                    <h6>ART BY</h6>
+                    <p class="text-primary">{{ implode(", ", $single_data['artists']) }}</p>
+                </div>
+                <div id="writers">
+                    <h6>WRITTEN BY</h6>
+                    <p class="text-primary">{{ implode(", ", $single_data['writers']) }}</p>
+                </div>
+            </div>
+        </div>
     </div>
     <div id="middle_section">
         <a href="{{ route('main') }}"> Go back to HOME PAGE</a>
